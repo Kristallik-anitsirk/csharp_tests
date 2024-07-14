ca72b14d0fe352b709c2356c6721fce7a3d3c71f
Kristallik <kristallik33@yandex.ru>
2024-07-14 13:53:17 +0300

    Первая выгрузка проекта

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitprogect
+{
+    class Program
+    {
+        public static void Main()
+        {
+            System.Console.WriteLine("Hello, GitHub!");
+        }
+    }
+}
